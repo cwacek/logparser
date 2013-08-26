09846d055c313ae24df0b498a73c0b62ad043cd8
Chris Wacek <cwacek@cs.georgetown.edu>
2013-08-26 17:39:51 -0400

    Improved documentation

--- logparser/parsers/__init__.py
+++ logparser/parsers/__init__.py
@@ -3,9 +3,19 @@ import functools
 import logging
 logger = logging.getLogger(__name__)
 
+__all__ = ['Parser', 'available_parsers']
+
 
 class Parser(object):
-  """Docstring for Parser """
+  """ Base class for parsers. All parsers
+  should define three member attributes:
+
+    :_name: The name of the parser
+    :_headers: The header fields for the data
+    :_data: The data itself, as a dictionary keyed by
+            :headers:
+
+  """
 
   def __init__(self):
     """Initialize a data object from this parser"""
@@ -40,8 +50,52 @@ def memoize(fn):
   return wrapper
 
 
+def __example_loader__(opts):
+
+  def example_parser_method(line):
+    pass
+  tmp = [('ex1', example_parser_method),
+         ('ex2', example_parser_method)]
+
+  for parser in tmp:
+    yield parser
+
+
 @memoize
 def available_parsers(opts):
+  """ Import all available parsers
+
+  Parsers are searched for in the logparser.parsers
+  namespace. A `parser' is considered to be any
+  callable which given a string returns a dictionary of
+  data parsed from it or raises a ValueError if the line
+  doesn't contain relevant data.
+
+  Since parsers are just callables, it's perfectly reasonable
+  to have them be methods on an instance. The same instance
+  will then be used on each line, allowing complicated parsers
+  which save state to be used
+
+  Parsers are loaded in one of two ways:
+
+  1. __loader__ method
+
+    If a package has a method called __loader__, it will
+    be called as a generator and passed a set of global
+    options. Each iteration should return tuples containing
+    names and parser callables.
+
+    >>> [(x[0], callable(x[1])) for x in __example_loader__({})]
+    [('ex1', True), ('ex2', True)]
+
+  2. __virtual__
+
+    A module can define a single parser by providing a
+    __virtual__ definition for the parser. This should be
+    a tuple containing the name of the parser and a
+    callable as described above.
+  """
+
   parsers = {}
   for loader, name, ispkg in pkgutil.iter_modules(__path__):
 
--- logparser/parsers/simpleregex.py
+++ logparser/parsers/simpleregex.py
@@ -58,12 +58,18 @@ def __loader__(opts):
 
 
 def parse_time(timestring, **opts):
-  """ Parse a string containing a timestamp into a float
+  """ Parse a string containing a timestamp into a float.
+  If the year is 1900 (i.e it's missing) it will set the
+  year to the current year.
 
   :timestring: The string
   :format: String format as used by datetime.strptime
   :returns: float
 
+  >>> parse_time('Aug 21 18:05:04.000')
+  1377108304
+  >>> parse_time('2/24/2012 4:25:22 PM', '%m/%d/%Y %I:%M:%S %p')
+  1330100722
   """
   import calendar
   from datetime import datetime as dt
@@ -78,15 +84,32 @@ def parse_time(timestring, **opts):
   return calendar.timegm(date.utctimetuple())
 
 
-def parse_list(string, **opts):
+def parse_list(string, strip=True, **opts):
   """ Parse a list from a string.
 
-  Defaults to string types with comma separation.
-  Also automatically strips each field before
-  conversion unless opts['strip'] is false.
+  Parse the list by splitting it apart based
+  on :separator:. If not defined, defaults to the
+  standard behavior of string.split(). Unless :strip:
+  is false, strip each split field.
 
-  :string: The string to parse
-  :opts: Field options, such as subtype, etc
+  Return a list containing each value, optionally
+  type-converted according to :subtype:. :subtype:
+  can be one of SimpleRegex.TYPES.
+
+  >>> parse_list('9 4 3 2 5')
+  ['9', '4', '3', '2', '5']
+
+  >>> parse_list('9 4 3 2 5', subtype='int')
+  [9, 4, 3, 2, 5]
+
+  >>> parse_list('9 4 3 2.4 5', subtype='float')
+  [9.0, 4.0, 3.0, 2.4, 5.0]
+
+  >>> parse_list('James Bond, Sherlock Holmes', split=',')
+  ['James Bond', 'Sherlock Holmes']
+
+  >>> parse_list('James Bond, Sherlock Holmes', split=',', strip=False)
+  ['James Bond', ' Sherlock Holmes']
   """
 
   sep = opts['separator'] if 'separator' in opts else None
@@ -106,10 +129,7 @@ def parse_list(string, **opts):
 
 
 class SimpleRegex(Parser):
-  """
-  This works a little differently than standard parsers which
-  create themselves with __init__. Here we create a parser,
-  then run it by calling run.
+  """ Load YAML defined Regex parsers.
   """
   TYPES = {
       'int': lambda x, **opts: int(x),
@@ -154,7 +174,7 @@ class SimpleRegex(Parser):
   def run(self, line):
     """ Actually run the parser. Return an object
       which has the data accessible via the property
-      :parsed: so that we behave pretty much like
+      :data: so that we behave pretty much like
       calling __init__ as a regular parser would.
     """
 
@@ -173,6 +193,6 @@ class SimpleRegex(Parser):
         raise TypeError("Failed to convert '{0}' to type '{1}'"
                         .format(matched, field['type']))
 
-    return OBJ(data=data)
+    return OBJ(data=data, name=self.name, headers=self.headers)
 
 __virtual__ = ('SimpleRegex', SimpleRegex)
