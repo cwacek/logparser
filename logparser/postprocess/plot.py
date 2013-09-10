4cebea016b3ba4de9e4a1b9acf5df64b2a7c64a9
Chris Wacek <cwacek@gmail.com>
2013-09-10 14:03:05 -0400

    Advanced plotting is mostly working

--- logparser/postprocess/plot.py
+++ logparser/postprocess/plot.py
@@ -1,5 +1,6 @@
 import logparser.postprocess.util as util
 import json
+from operator import itemgetter
 
 import logging
 logger = logging.getLogger(__name__)
@@ -30,17 +31,21 @@ def get_ggplot_args(argparse_args):
   :argparse_args: cmdline parsed args
   :returns: util.Namespace with ggplot args in it
   """
-  ggplot_names = ('x', 'y', 'type')
-  ggplot_args = dict(((arg, None) for arg in ggplot_names))
+  ggplot_names = dict((('x', 'aes.x'),
+                       ('y', 'aes.y'),
+                       ('aes.col', 'aes.col'),
+                       ('type', 'type'),
+                       ('col', 'col')))
+  ggplot_args = dict(((arg, None) for arg in ggplot_names.itervalues()))
   global_vars = util.Namespace({
       'title': 'LogParser Graph'
   })
 
   for argname in argparse_args.__dict__:
     if argname in ggplot_names:
-      ggplot_args[argname] = getattr(argparse_args, argname)
+      ggplot_args[ggplot_names[argname]] = getattr(argparse_args, argname)
 
-  global_vars.set('facets', argparse_args.__dict__.get('facets', None))
+  global_vars.set('facets', getattr(argparse_args, 'facets', None))
 
   return (global_vars, {'cmdline': util.Namespace(ggplot_args)})
 
@@ -80,7 +85,10 @@ def plot(args):
         logging.warn("Error saving plot: {0}".format(io))
     except StopIteration:
       return
-    have_plot = render_plot(gp, geometries, global_vars)
+    try:
+      have_plot = render_plot(gp, global_vars, geometries)
+    except Exception as e:
+      logging.warn("Error: {0}".format(e))
 
 
 def geometry_prompt(current=None):
@@ -91,18 +99,18 @@ def geometry_prompt(current=None):
     print("Creating new geometry")
     current = util.Namespace({})
 
-  current.set('x',
-              util.prompt('x-axis', current.get('x', None))) \
-         .set('y',
-              util.prompt('y-axis', current.get('y', None))) \
-         .set('col.aes',
+  current.set('aes.x',
+              util.prompt('x-axis', current.get('aes.x', None))) \
+         .set('aes.y',
+              util.prompt('y-axis', current.get('aes.y', None))) \
+         .set('aes.col',
               util.prompt('data-varied color',
-                          current.get('col.aes', None))) \
-         .set('col.fixed',
+                          current.get('aes.col', None))) \
+         .set('col',
               util.prompt('fixed color',
-                          current.get('col.fixed', None)))   \
+                          current.get('col', None)))   \
          .set('type',
-              util.prompt('type (line, point, smooth)',
+              util.prompt('type (lines, points, smooth)',
                           current.get('type', None)))
 
   return current
@@ -206,38 +214,70 @@ def print_menu(global_vars, geometries, dataframe, have_plot):
         print("Couldn't parse row number")
 
     if cmd[0] in ('replot', 'plot'):
-      return geometries
+      return global_vars, geometries
 
     if cmd[0] == 'save':
       raise SavePlotException(cmd[1])
 
+GGPLOT_TYPEFUNC = {
+    'points': ggplot2.geom_point,
+    'lines': ggplot2.geom_line,
+    'smooth': ggplot2.geom_smooth
+}
 
-def render_plot(gp, args):
+
+def render_plot(gp, plotvars, geoms):
   """Render a plot using ggplot
 
   :gp: A base ggplot2 object
-  :x: The x value expression
-  :y: The y value expression
-  :type: The type of plot to make
-
+  :geoms: A dictionary of labeled geometries
+  :plotvars: Global parameters for the graph
   """
-  args = util.Namespace(args)
-
   import rpy2.robjects.lib.ggplot2 as ggplot2
 
-  pp = gp + ggplot2.aes_string(x=args.x,
-                               y=args.y)
+  pp = gp + ggplot2.ggtitle(plotvars.title) \
 
-  if args.type == 'points':
-    pp += ggplot2.geom_point()
-  elif args.type == 'lines':
-    pp += ggplot2.geom_line()
-  elif args.type == 'boxplot':
-    pp += ggplot2.geom_boxplot()
+# use the first geometry to set the aes and other things
+  tmpgeoms = util.Namespace(geoms)
+  geom = tmpgeoms.popitem()[1]
+  aes_args = {key.lstrip('aes.'): val
+              for key, val in geom.iteritems()
+              if key.startswith('aes.') and val is not None}
+
+  geom_args = {}
+  if geom.get('col', None) is not None:
+    geom_args['col'] = geom['col']
+
+  pp += ggplot2.aes(**aes_args)
+  pp += GGPLOT_TYPEFUNC[geom.get('type')](**geom_args)
+
+  if 'xlab' in plotvars:
+    pp += ggplot2.ggplot2.xlab(plotvars['xlab'])
   else:
-    raise Exception("{0} not implemented".format(args.type))
+    pp += ggplot2.ggplot2.xlab(geom.get('aes.x'))
 
-  if args.facets is not None:
+  if 'ylab' in plotvars:
+    pp += ggplot2.ggplot2.ylab(plotvars['ylab'])
+  else:
+    pp += ggplot2.ggplot2.ylab(geom.get('aes.y'))
+
+  for geom in tmpgeoms.itervalues():
+    args = {key.lstrip('aes.'): val
+            for key, val in geom.iteritems()
+            if key.startswith('aes.') and val is not None}
+
+    try:
+      geom_args = {}
+      if geom.get('col', None) is not None:
+        geom_args['col'] = geom['col']
+
+      pp += GGPLOT_TYPEFUNC[geom.get('type')](
+          ggplot2.aes_string(**args), **geom_args)
+    except KeyError:
+      raise KeyError("Plot type '{0}' is not supported."
+                     .format(geom.get('type')))
+
+  if plotvars.get('facets', None) is not None:
     try:
       pp += ggplot2.facet_grid(ro.Formula(args.facets))
     except Exception:
